f8d51c9103800c4f956b70800ae231d36631251f
Cezary Żelisko <cezary.zelisko@gmail.com>
2020-05-27 18:48:49 +0200

    - dummy GPS locator modified to meet new API requirements, - models registered to admin console, - __str__ for models, - serializers added, - urls modified, - views for serializers.

--- utils/dummy_gps_locator.py
+++ utils/dummy_gps_locator.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 import time
 from argparse import ArgumentParser
@@ -7,6 +8,7 @@ import requests
 from gps_tracker.utils.config_parser import parse_config
 
 STARTING_COORDS = (51.005, 21.001)
+DEVICE_ID = 1
 
 
 def get_csrf_token(config):
@@ -23,23 +25,31 @@ def get_new_coords(abs_diff=0.01, prev_coords=None):
         return prev_coords[0] + random.choice([-1, 1]) * abs_diff, prev_coords[1] + random.choice([-1, 1]) * abs_diff
 
 
-def publish_new_coords(interval, config, csrf_token):
+def publish_new_coords(interval, config, csrf_token=None):
     coords = STARTING_COORDS
     gps_endpoint = '/'.join([config['api_root'], config['endpoints']['gps_footprint']])
-    headers = {'X-CSRFToken': csrf_token}
-    cookies = {'csrftoken': csrf_token}
+    if csrf_token:
+        headers = {'X-CSRFToken': csrf_token}
+        cookies = {'csrftoken': csrf_token}
+    else:
+        headers = None
+        cookies = None
     try:
         while True:
             coords = get_new_coords(prev_coords=coords)
-            published_at = time.time()
-            print(f'{published_at}\tlat:\t{coords[0]}, lng:\t{coords[1]}')
+            published_at = datetime.datetime.now()
+            published_at_str = f'{published_at.year}-{published_at.month}-{published_at.day} ' \
+                               f'{published_at.hour}:{published_at.minute}:{published_at.second}'
             body = {
                 'lat': coords[0],
                 'lng': coords[1],
-                'published_at': published_at
+                'published_at': published_at_str,
+                'device_id': DEVICE_ID
             }
             response = requests.post(gps_endpoint, body, headers=headers, cookies=cookies)
-            print(response)
+            print(f'[POST] -> '
+                  f'{published_at_str}, lat: {coords[0]:.3f}, lng: {coords[1]:.3f}, device_id: {DEVICE_ID} -> '
+                  f'{response.status_code}')
             time.sleep(interval)
     except KeyboardInterrupt:
         print('exiting...')
@@ -52,6 +62,6 @@ if __name__ == '__main__':
 
     config = parse_config()
 
-    csrf_token = get_csrf_token(config)
+    # csrf_token = get_csrf_token(config)
 
-    publish_new_coords(args.interval, config, csrf_token)
+    publish_new_coords(args.interval, config)
